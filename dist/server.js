c87ece06fdabec6ddf98198411faf9e17b62ca00
banyq <snx1788@gmail.com>
2019-02-05 14:53:35 +0800

    init1

--- dist/server.js
+++ dist/server.js
@@ -1,3 +1,19 @@
-console.log('server.js executed..');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var http = require('http');
 var PORT = process.env.npm_package_server_port;
-console.log(PORT);
+http.createServer(function (request, response) {
+    console.log("received request", request.method);
+    response.end("hello world");
+}).listen(PORT);
+console.log("server is running");
+var o = {};
+Object.defineProperty(o, 'test', {
+    get: function () {
+        return 1;
+    },
+    set: function (x) {
+        this.test = x;
+    }
+});
+//# sourceMappingURL=server.js.map
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,22 @@
-console.log('server.js executed..');
+import { ServerResponse, ClientRequest } from "http";
 
-
+const http = require('http');
 const PORT = process.env.npm_package_server_port;
 
-console.log(PORT);
+http.createServer((request: Request, response: ServerResponse) => {
+    console.log("received request", request.method);
+    response.end("hello world");
+}).listen(PORT);
+
+console.log("server is running");
+
+
+let o = {};
+Object.defineProperty(o, 'test', {
+    get() {
+        return 1;
+    },
+    set(x) {
+        this.test = x;
+    }
+})
